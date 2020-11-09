f98cfde21172f21bc1fd899773b1e67dc741818a
rpratama94 <rpratama.sky@gmail.com>
2020-11-09 18:12:39 +0700

    Add function Login UserController

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,4 +1,7 @@
 const { User } = require('../models');
+const createError = require('http-errors');
+const { comparePassword } = require('../helpers/bcrypt');
+const { generateToken } = require('../helpers/jwt');
 
 class UserController {
 
@@ -16,6 +19,34 @@ class UserController {
       next(error);
     }
   }
+
+  static async login(req, res, next) {
+    const inputEmail = req.body.email;
+    const inputPassword = req.body.password;
+    const message = "Wrong Username / Password";
+
+    try {
+      const user = await User.findOne({ where: { email: inputEmail } });
+      if (!user) {
+        throw (createError(401, message))
+      } else {
+        const { id, fullname, email, password } = user;
+        const match = comparePassword(inputPassword, password);
+        
+        if (!match) {
+          throw (createError(401, message));
+        } else {
+          const payload = { id, fullname, email };
+          const access_token = generateToken(payload);
+          res.status(200).json({ status: 200, user: payload, access_token });
+        }
+      }
+    } catch (error) {
+      next(error)
+    }
+  }
 }
+
+
 
 module.exports = UserController;
