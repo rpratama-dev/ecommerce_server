893e6a84b40feab996114cbac1dfc5dc100ad9bc
rpratama94 <rpratama.sky@gmail.com>
2020-11-19 11:39:22 +0700

    Add google login

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,6 +2,7 @@ const { User } = require('../models');
 const createError = require('http-errors');
 const { comparePassword } = require('../helpers/bcrypt');
 const { generateToken } = require('../helpers/jwt');
+const { OAuth2Client } = require('google-auth-library');
 
 class UserController {
 
@@ -44,6 +45,67 @@ class UserController {
     } catch (error) {
       next(error)
     }
+  }
+
+  static async googlesignin(req, res, next) {
+    let { google_access_token } = req.body;
+    const client = new OAuth2Client(process.env.CLIENT_ID);
+    let userGoogle = {}
+
+    try {
+      const ticket = await client.verifyIdToken({
+        idToken: google_access_token,
+        audience: process.env.CLIENT_ID,
+      })
+      const payload = ticket.getPayload();
+      const { name, email, picture } = payload;
+      userGoogle = { name, email, picture }
+
+      const user = User.findOne({ where: { email } })
+      if (user) {
+        const { id, fullname, email } = user
+        const payload = { id, fullname, email };
+        const access_token = generateToken(payload);
+        payload.role = role;
+        res.status(200).json({ status: 200, user: payload, access_token });
+      } else {
+        const newUser = {
+          fullname: userGoogle.name,
+          password: 'jJys8Hsk8wEmJSa',
+          email: userGoogle.email
+        }
+        const user = await User.create(newUser)
+      }
+    } catch (error) {
+      next(error)
+    }
+
+    // client.verifyIdToken({
+    //   idToken: google_access_token,
+    //   audience: process.env.CLIENT_ID,
+    // }).then(ticket => {
+    //   const payload = ticket.getPayload();
+    //   const { name, email, picture } = payload;
+    //   userGoogle = { name, email, picture }
+    //   return User.findOne({ where: { email } })
+    // }).then(user => {
+    //   if (user) {
+    //     return user
+    //   } else {
+    //     const newUser = {
+    //       fullname: userGoogle.name,
+    //       password: 'jJys8Hsk8wEmJSa',
+    //       email: userGoogle.email
+    //     }
+    //     return User.create(newUser)
+    //   }
+    // }).then(data => {
+    //   const { id, fullname, email } = data;
+    //   const access_token = generateToken({ id, email, fullname });
+    //   res.status(200).json({ id, email, fullname, access_token });
+    // }).catch(err => {
+    //   next(err)
+    // })
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,7 @@ router.get("/", (req, res, next) => {
 
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
+router.post('/googlesignin', UserController.googlesignin);
 
 router.use('/products', productRouter)
 router.use('/categories', categoryRouter)
